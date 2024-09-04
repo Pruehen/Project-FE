40d9af1ea721a4f75cb09beeba6531222b310514
JihoKwon <forjiho7@gmail.com>
2024-09-04 16:23:50 +0900

    [Feat] 건물, 벨트, 투입기, 광맥의 상호작용 기능 정리 및 프리팹 추가

---
+++ Assets/Scripts/Interactable/Inserter.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inserter : MonoBehaviour, IInteractable
+{
+    [SerializeField] string ItemKey;
+    [SerializeField] string BuildingKey;
+
+    BuildingData _buildingData;
+    IModule _MainModule;
+
+    Outline _outline;
+    Outline Outline
+    {
+        get
+        {
+            if (_outline == null)
+                _outline = GetComponent<Outline>();
+            return _outline;
+        }
+    }
+
+    public BuildingData BuildingData
+    {
+        get
+        {
+            if (_buildingData == null)
+            {
+                _buildingData = JsonDataManager.GetBuilding(BuildingKey);
+            }
+            return _buildingData;
+        }
+    }
+    public List<string> RecipyGroupData
+    {
+        get
+        {
+            return JsonDataManager.GetRecipyGroupData(BuildingData.RecipyGroup);
+        }
+    }
+
+    public string GetName()
+    {
+        ItemData data = JsonDataManager.GetItem(ItemKey);
+        if (data != null)
+        {
+            string name = data.Name;
+            return name;
+        }
+        else
+        {
+            return "키를 찾을 수 없음";
+        }
+    }
+    public Vector3 GetPos()
+    {
+        return this.transform.position;
+    }
+    public float InteractSpeedGain()
+    {
+        return 1;
+    }
+    public void Select()
+    {
+        UIManager.Instance.Active_BuildingMainModuleUIWdw(_MainModule);
+    }
+
+    public void MouseEnter()
+    {
+        Outline.IsOutlineEnabled = true;
+    }
+
+    public void MouseExit()
+    {
+        Outline.IsOutlineEnabled = false;
+    }
+
+    public bool TryInteract(Vector3 originPos, float checkRange)
+    {
+        if (Vector3.Distance(originPos, GetPos()) > checkRange)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void Awake()
+    {
+        _MainModule = GetComponent<IModule>();
+    }
+}
---
+++ Assets/Scripts/Module/MinerModule.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinerModule : MonoBehaviour, IModule
+{
+    public void Active_Wdw()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Close_Wdw()
+    {
+        throw new System.NotImplementedException();
+    }
+}
